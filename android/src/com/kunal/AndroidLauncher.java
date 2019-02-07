5f734e2dad54ac597ca1e911d127670d580fccc7
Kunal Sagar <kunalsagar1999@gmail.com>
2019-02-07 22:30:55 +0530

    admob added

--- android/src/com/kunal/AndroidLauncher.java
+++ android/src/com/kunal/AndroidLauncher.java
@@ -10,6 +10,7 @@ public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 
+
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new MainGame(), config);
