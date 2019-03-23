8750ea89db8b85e870aa3a73fb795722e8142397
Kunal Sagar <kunalsagar1999@gmail.com>
2019-03-23 16:18:09 +0530

    tiled map obj created

--- core/src/com/kunal/PlayGround/PlayAreaUtils.java
+++ core/src/com/kunal/PlayGround/PlayAreaUtils.java
@@ -1,8 +1,15 @@
 package com.kunal.PlayGround;
 
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.MapObjects;
+import com.badlogic.gdx.maps.objects.PolylineMapObject;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.ChainShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.Shape;
+import com.badlogic.gdx.physics.box2d.World;
 import com.kunal.AllVariables;
 import com.kunal.utils.BodyGenerator;
 
@@ -27,6 +34,39 @@ public class PlayAreaUtils {
             b.createFixture(fdef);
 
         }
+    }
+
+
+    public static void parseTiledObj(World world, MapObjects objects){
+        for (MapObject object : objects){
+            Shape shape;
+            if(object instanceof PolylineMapObject){
+                shape = createPolyLine((PolylineMapObject)object);
+            }else{
+                continue;
+            }
+
+            Body body;
+            BodyDef bdef = new BodyDef();
+            bdef.type = BodyDef.BodyType.StaticBody;
+            body = world.createBody(bdef);
+            body.createFixture(shape, 1f);
+            shape.dispose();
+        }
+    }
+
+    public static ChainShape createPolyLine(PolylineMapObject polyline){
+        float[] vertices = polyline.getPolyline().getTransformedVertices();
+        Vector2[] worldVertices = new Vector2[vertices.length/2];
+
+        for (int i=0; i < worldVertices.length; i++){
+            worldVertices[i] = new Vector2(vertices[i*2]/AllVariables.PPM, vertices[i*2+1]/AllVariables.PPM);
+        }
+
+        ChainShape cs = new ChainShape();
+        cs.createChain(worldVertices);
+
+        return cs;
     }
 
 
