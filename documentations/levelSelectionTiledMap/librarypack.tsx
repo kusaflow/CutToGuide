18b0143a2a3d6941f59cdec9ce1bda4744fe3899
Kunal Sagar <kunalsagar1999@gmail.com>
2019-04-03 21:42:37 +0530

    tiledmap created

---
+++ documentations/levelSelectionTiledMap/librarypack.tsx
@@ -0,0 +1,436 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="librarypack" tilewidth="256" tileheight="512" tilecount="144" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseBooks_E.png"/>
+ </tile>
+ <tile id="1">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseBooks_N.png"/>
+ </tile>
+ <tile id="2">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseBooks_S.png"/>
+ </tile>
+ <tile id="3">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseBooks_W.png"/>
+ </tile>
+ <tile id="4">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseBooksLadder_E.png"/>
+ </tile>
+ <tile id="5">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseBooksLadder_N.png"/>
+ </tile>
+ <tile id="6">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseBooksLadder_S.png"/>
+ </tile>
+ <tile id="7">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseBooksLadder_W.png"/>
+ </tile>
+ <tile id="8">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseDestroyed_E.png"/>
+ </tile>
+ <tile id="9">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseDestroyed_N.png"/>
+ </tile>
+ <tile id="10">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseDestroyed_S.png"/>
+ </tile>
+ <tile id="11">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseDestroyed_W.png"/>
+ </tile>
+ <tile id="12">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseEmpty_E.png"/>
+ </tile>
+ <tile id="13">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseEmpty_N.png"/>
+ </tile>
+ <tile id="14">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseEmpty_S.png"/>
+ </tile>
+ <tile id="15">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseEmpty_W.png"/>
+ </tile>
+ <tile id="16">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseEmptyLadder_E.png"/>
+ </tile>
+ <tile id="17">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseEmptyLadder_N.png"/>
+ </tile>
+ <tile id="18">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseEmptyLadder_S.png"/>
+ </tile>
+ <tile id="19">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseEmptyLadder_W.png"/>
+ </tile>
+ <tile id="20">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseFallen_E.png"/>
+ </tile>
+ <tile id="21">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseFallen_N.png"/>
+ </tile>
+ <tile id="22">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseFallen_S.png"/>
+ </tile>
+ <tile id="23">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseFallen_W.png"/>
+ </tile>
+ <tile id="24">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseGlass_E.png"/>
+ </tile>
+ <tile id="25">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseGlass_N.png"/>
+ </tile>
+ <tile id="26">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseGlass_S.png"/>
+ </tile>
+ <tile id="27">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseGlass_W.png"/>
+ </tile>
+ <tile id="28">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseHalfBooks_E.png"/>
+ </tile>
+ <tile id="29">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseHalfBooks_N.png"/>
+ </tile>
+ <tile id="30">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseHalfBooks_S.png"/>
+ </tile>
+ <tile id="31">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseHalfBooks_W.png"/>
+ </tile>
+ <tile id="32">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseHalfEmpty_E.png"/>
+ </tile>
+ <tile id="33">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseHalfEmpty_N.png"/>
+ </tile>
+ <tile id="34">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseHalfEmpty_S.png"/>
+ </tile>
+ <tile id="35">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseHalfEmpty_W.png"/>
+ </tile>
+ <tile id="36">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseHalfWideBooks_E.png"/>
+ </tile>
+ <tile id="37">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseHalfWideBooks_N.png"/>
+ </tile>
+ <tile id="38">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseHalfWideBooks_S.png"/>
+ </tile>
+ <tile id="39">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseHalfWideBooks_W.png"/>
+ </tile>
+ <tile id="40">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseHalfWideEmpty_E.png"/>
+ </tile>
+ <tile id="41">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseHalfWideEmpty_N.png"/>
+ </tile>
+ <tile id="42">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseHalfWideEmpty_S.png"/>
+ </tile>
+ <tile id="43">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseHalfWideEmpty_W.png"/>
+ </tile>
+ <tile id="44">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseWideBooks_E.png"/>
+ </tile>
+ <tile id="45">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseWideBooks_N.png"/>
+ </tile>
+ <tile id="46">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseWideBooks_S.png"/>
+ </tile>
+ <tile id="47">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseWideBooks_W.png"/>
+ </tile>
+ <tile id="48">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseWideBooksDesk_E.png"/>
+ </tile>
+ <tile id="49">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseWideBooksDesk_N.png"/>
+ </tile>
+ <tile id="50">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseWideBooksDesk_S.png"/>
+ </tile>
+ <tile id="51">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseWideBooksDesk_W.png"/>
+ </tile>
+ <tile id="52">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseWideBooksLadder_E.png"/>
+ </tile>
+ <tile id="53">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseWideBooksLadder_N.png"/>
+ </tile>
+ <tile id="54">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseWideBooksLadder_S.png"/>
+ </tile>
+ <tile id="55">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseWideBooksLadder_W.png"/>
+ </tile>
+ <tile id="56">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseWideEmpty_E.png"/>
+ </tile>
+ <tile id="57">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseWideEmpty_N.png"/>
+ </tile>
+ <tile id="58">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseWideEmpty_S.png"/>
+ </tile>
+ <tile id="59">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseWideEmpty_W.png"/>
+ </tile>
+ <tile id="60">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseWideEmptyLadder_E.png"/>
+ </tile>
+ <tile id="61">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseWideEmptyLadder_N.png"/>
+ </tile>
+ <tile id="62">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseWideEmptyLadder_S.png"/>
+ </tile>
+ <tile id="63">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookcaseWideEmptyLadder_W.png"/>
+ </tile>
+ <tile id="64">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookStand_E.png"/>
+ </tile>
+ <tile id="65">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookStand_N.png"/>
+ </tile>
+ <tile id="66">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookStand_S.png"/>
+ </tile>
+ <tile id="67">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookStand_W.png"/>
+ </tile>
+ <tile id="68">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookStandEmpty_E.png"/>
+ </tile>
+ <tile id="69">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookStandEmpty_N.png"/>
+ </tile>
+ <tile id="70">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookStandEmpty_S.png"/>
+ </tile>
+ <tile id="71">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/bookStandEmpty_W.png"/>
+ </tile>
+ <tile id="72">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/candleStand_E.png"/>
+ </tile>
+ <tile id="73">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/candleStand_N.png"/>
+ </tile>
+ <tile id="74">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/candleStand_S.png"/>
+ </tile>
+ <tile id="75">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/candleStand_W.png"/>
+ </tile>
+ <tile id="76">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/candleStandDouble_E.png"/>
+ </tile>
+ <tile id="77">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/candleStandDouble_N.png"/>
+ </tile>
+ <tile id="78">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/candleStandDouble_S.png"/>
+ </tile>
+ <tile id="79">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/candleStandDouble_W.png"/>
+ </tile>
+ <tile id="80">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/displayCase_E.png"/>
+ </tile>
+ <tile id="81">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/displayCase_N.png"/>
+ </tile>
+ <tile id="82">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/displayCase_S.png"/>
+ </tile>
+ <tile id="83">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/displayCase_W.png"/>
+ </tile>
+ <tile id="84">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/displayCaseBooks_E.png"/>
+ </tile>
+ <tile id="85">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/displayCaseBooks_N.png"/>
+ </tile>
+ <tile id="86">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/displayCaseBooks_S.png"/>
+ </tile>
+ <tile id="87">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/displayCaseBooks_W.png"/>
+ </tile>
+ <tile id="88">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/displayCaseOpen_E.png"/>
+ </tile>
+ <tile id="89">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/displayCaseOpen_N.png"/>
+ </tile>
+ <tile id="90">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/displayCaseOpen_S.png"/>
+ </tile>
+ <tile id="91">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/displayCaseOpen_W.png"/>
+ </tile>
+ <tile id="92">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/displayCaseSword_E.png"/>
+ </tile>
+ <tile id="93">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/displayCaseSword_N.png"/>
+ </tile>
+ <tile id="94">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/displayCaseSword_S.png"/>
+ </tile>
+ <tile id="95">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/displayCaseSword_W.png"/>
+ </tile>
+ <tile id="96">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/floorCarpet_E.png"/>
+ </tile>
+ <tile id="97">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/floorCarpet_N.png"/>
+ </tile>
+ <tile id="98">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/floorCarpet_S.png"/>
+ </tile>
+ <tile id="99">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/floorCarpet_W.png"/>
+ </tile>
+ <tile id="100">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/floorCarpetEnd_E.png"/>
+ </tile>
+ <tile id="101">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/floorCarpetEnd_N.png"/>
+ </tile>
+ <tile id="102">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/floorCarpetEnd_S.png"/>
+ </tile>
+ <tile id="103">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/floorCarpetEnd_W.png"/>
+ </tile>
+ <tile id="104">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/floorCarpetSmall_E.png"/>
+ </tile>
+ <tile id="105">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/floorCarpetSmall_N.png"/>
+ </tile>
+ <tile id="106">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/floorCarpetSmall_S.png"/>
+ </tile>
+ <tile id="107">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/floorCarpetSmall_W.png"/>
+ </tile>
+ <tile id="108">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/libraryChair_E.png"/>
+ </tile>
+ <tile id="109">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/libraryChair_N.png"/>
+ </tile>
+ <tile id="110">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/libraryChair_S.png"/>
+ </tile>
+ <tile id="111">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/libraryChair_W.png"/>
+ </tile>
+ <tile id="112">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/longTable_E.png"/>
+ </tile>
+ <tile id="113">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/longTable_N.png"/>
+ </tile>
+ <tile id="114">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/longTable_S.png"/>
+ </tile>
+ <tile id="115">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/longTable_W.png"/>
+ </tile>
+ <tile id="116">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/longTableChairs_E.png"/>
+ </tile>
+ <tile id="117">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/longTableChairs_N.png"/>
+ </tile>
+ <tile id="118">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/longTableChairs_S.png"/>
+ </tile>
+ <tile id="119">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/longTableChairs_W.png"/>
+ </tile>
+ <tile id="120">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/longTableDecorated_E.png"/>
+ </tile>
+ <tile id="121">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/longTableDecorated_N.png"/>
+ </tile>
+ <tile id="122">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/longTableDecorated_S.png"/>
+ </tile>
+ <tile id="123">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/longTableDecorated_W.png"/>
+ </tile>
+ <tile id="124">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/longTableDecoratedChairs_E.png"/>
+ </tile>
+ <tile id="125">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/longTableDecoratedChairs_N.png"/>
+ </tile>
+ <tile id="126">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/longTableDecoratedChairs_S.png"/>
+ </tile>
+ <tile id="127">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/longTableDecoratedChairs_W.png"/>
+ </tile>
+ <tile id="128">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/longTableDecoratedChairsBooks_E.png"/>
+ </tile>
+ <tile id="129">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/longTableDecoratedChairsBooks_N.png"/>
+ </tile>
+ <tile id="130">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/longTableDecoratedChairsBooks_S.png"/>
+ </tile>
+ <tile id="131">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/longTableDecoratedChairsBooks_W.png"/>
+ </tile>
+ <tile id="132">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/longTableLarge_E.png"/>
+ </tile>
+ <tile id="133">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/longTableLarge_N.png"/>
+ </tile>
+ <tile id="134">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/longTableLarge_S.png"/>
+ </tile>
+ <tile id="135">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/longTableLarge_W.png"/>
+ </tile>
+ <tile id="136">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/wallBooks_E.png"/>
+ </tile>
+ <tile id="137">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/wallBooks_N.png"/>
+ </tile>
+ <tile id="138">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/wallBooks_S.png"/>
+ </tile>
+ <tile id="139">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/wallBooks_W.png"/>
+ </tile>
+ <tile id="140">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/wallDoorway_E.png"/>
+ </tile>
+ <tile id="141">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/wallDoorway_N.png"/>
+ </tile>
+ <tile id="142">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/wallDoorway_S.png"/>
+ </tile>
+ <tile id="143">
+  <image width="256" height="512" source="../tiles/library-pack-2.1/Isometric/wallDoorway_W.png"/>
+ </tile>
+</tileset>
