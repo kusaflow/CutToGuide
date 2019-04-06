f4c7abaf96641377bded687c598fe059ed193f89
Kunal Sagar <kunalsagar1999@gmail.com>
2019-04-06 23:40:14 +0530

    level Selection

--- core/src/com/kunal/AreaSelection/levelSelection.java
+++ core/src/com/kunal/AreaSelection/levelSelection.java
@@ -1,4 +1,48 @@
 package com.kunal.AreaSelection;
 
-public class levelSelection {
+import com.badlogic.gdx.Screen;
+import com.kunal.MainGame;
+
+public class levelSelection implements Screen {
+
+    MainGame game;
+
+    public levelSelection(MainGame game) {
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
