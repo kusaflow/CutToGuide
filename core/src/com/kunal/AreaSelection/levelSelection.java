8eb88cd044521af87526bef4c193e7052513f2ec
Kunal Sagar <kunalsagar1999@gmail.com>
2019-04-15 19:43:57 +0530

    tried again

--- core/src/com/kunal/AreaSelection/levelSelection.java
+++ core/src/com/kunal/AreaSelection/levelSelection.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.kunal.AllVariables;
@@ -17,6 +18,8 @@ public class levelSelection implements Screen {
     Viewport port;
     Texture levelArea1;
 
+    Sprite l1;
+
     public levelSelection(MainGame game) {
         this.game = game;
 
@@ -27,7 +30,10 @@ public class levelSelection implements Screen {
 
         port.apply();
 
-        levelArea1 = new Texture(Gdx.files.internal("levelSelection/p1.png"));
+        //levelArea1 = new Texture(Gdx.files.internal("levelSelection/p1.png"));
+        l1 = new Sprite(new Texture(Gdx.files.internal("levelSelection/p1.png")));
+        l1.setSize(AllVariables.WIDTH, AllVariables.HEIGHT);
+        l1.setPosition(0,0);
 
         AllVariables.inpM = (float)Gdx.graphics.getHeight()/AllVariables.HEIGHT;
         AllVariables.witdth_translation =  (Gdx.graphics.getWidth() - ((Gdx.graphics.getHeight()*16)/9))/2;
@@ -46,7 +52,10 @@ public class levelSelection implements Screen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         AllVariables.batch.begin();
-        AllVariables.batch.draw(levelArea1,0,0, AllVariables.WIDTH/1.33f, AllVariables.HEIGHT/1.33f);
+        /*AllVariables.batch.draw(levelArea1,0,0, Gdx.graphics.getWidth()3.
+            , Gdx.graphics.getHeight());
+        */
+        l1.draw(AllVariables.batch);
         AllVariables.batch.end();
 
 
