41961ab829e2c59772c22d59296c83bebaa668da
Kunal Sagar <kunalsagar1999@gmail.com>
2019-05-09 23:19:46 +0530

    layed down cancel and free ad

--- core/src/com/kunal/Shop/Shop.java
+++ core/src/com/kunal/Shop/Shop.java
@@ -4,6 +4,12 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.kunal.AllVariables;
 import com.kunal.MainGame;
 
 public class Shop implements Screen {
@@ -11,14 +17,44 @@ public class Shop implements Screen {
     MainGame game;
     Screen prevScreen;
 
+    OrthographicCamera cam;
+    Viewport port;
+
+    Sprite freeAd, buy1, buy2, buy3, buy4, buy5, cancel;
+
+
     public Shop(MainGame game, Screen PrevScreen) {
         this.game = game;
         prevScreen = PrevScreen;
+
+        cam = new OrthographicCamera();
+        cam.setToOrtho(false, AllVariables.WIDTH, AllVariables.HEIGHT);
+
+        port = new FitViewport(AllVariables.WIDTH, AllVariables.HEIGHT, cam);
+
+        freeAd = new Sprite(new Texture(Gdx.files.internal("Shop/FreeForAd.png")));
+        freeAd.setSize(350,350);
+        freeAd.setPosition(30,350);
+
+        cancel= new Sprite(new Texture(Gdx.files.internal("Shop/cancel.png")));
+        cancel.setSize(200,200);
+        cancel.setPosition(30,20);
+
+        buy1= new Sprite(new Texture(Gdx.files.internal("Shop/buy1.png")));
+        buy1.setSize(200,200);
+        buy1.setPosition(550,600);
+
+
+
+        AllVariables.inpM = (float)Gdx.graphics.getHeight()/AllVariables.HEIGHT;
+        AllVariables.witdth_translation =  (Gdx.graphics.getWidth() - ((Gdx.graphics.getHeight()*16)/9))/2;
+
     }
 
     @Override
     public void show() {
-
+        AllVariables.inpM = (float)Gdx.graphics.getHeight()/AllVariables.HEIGHT;
+        AllVariables.witdth_translation =  (Gdx.graphics.getWidth() - ((Gdx.graphics.getHeight()*16)/9))/2;
     }
 
     @Override
@@ -26,6 +62,14 @@ public class Shop implements Screen {
         update();
         Gdx.gl.glClearColor(.1f, .1f, .1f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        AllVariables.batch.setProjectionMatrix(cam.combined);
+
+        AllVariables.batch.begin();
+        cancel.draw(AllVariables.batch);
+        freeAd.draw(AllVariables.batch);
+        buy1.draw(AllVariables.batch);
+        AllVariables.batch.end();
 
     }
 
@@ -42,7 +86,10 @@ public class Shop implements Screen {
 
     @Override
     public void resize(int width, int height) {
-
+        port.update(width, height);
+        cam.update();
+        AllVariables.inpM = (float)Gdx.graphics.getHeight()/AllVariables.HEIGHT;
+        AllVariables.witdth_translation =  (Gdx.graphics.getWidth() - ((Gdx.graphics.getHeight()*16)/9))/2;
     }
 
     @Override
