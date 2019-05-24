a98301a7f940a908d9e9ee0252a9e5beb9afbb28
Kunal Sagar <kunalsagar1999@gmail.com>
2019-05-24 11:57:02 +0530

    fileSystemForLevelArea

--- core/src/com/kunal/AreaSelection/levelNumberSelection/LevelNumberSelection.java
+++ core/src/com/kunal/AreaSelection/levelNumberSelection/LevelNumberSelection.java
@@ -1,21 +1,36 @@
 package com.kunal.AreaSelection.levelNumberSelection;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.kunal.AllVariables;
+import com.kunal.AreaSelection.AreaSelection;
 import com.kunal.MainGame;
 
 public class LevelNumberSelection implements Screen {
     MainGame game;
+    OrthographicCamera cam;
+    Viewport port;
+
 
     public LevelNumberSelection(MainGame game) {
         this.game = game;
+        cam = new OrthographicCamera();
+        cam.setToOrtho(false, AllVariables.WIDTH, AllVariables.HEIGHT);
+
+        port = new FitViewport(AllVariables.WIDTH, AllVariables.HEIGHT, cam);
+
         System.out.println(AllVariables.PresentAreaNumber);
     }
 
     @Override
     public void show() {
+        AllVariables.inpM = (float)Gdx.graphics.getHeight()/AllVariables.HEIGHT;
+        AllVariables.witdth_translation =  (Gdx.graphics.getWidth() - ((Gdx.graphics.getHeight()*16)/9))/2;
 
     }
 
@@ -24,11 +39,21 @@ public class LevelNumberSelection implements Screen {
         Gdx.gl.glClearColor(0.5f, 0.4f, 0.3f, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        input(delta);
+
+    }
+
+    private void input(float dt){
+        if (Gdx.input.isKeyJustPressed(Input.Keys.B))
+            game.setScreen(new AreaSelection(game));
     }
 
     @Override
     public void resize(int width, int height) {
-
+        //port.update(width, height);
+        //cam.update();
+        AllVariables.inpM = (float)Gdx.graphics.getHeight()/AllVariables.HEIGHT;
+        AllVariables.witdth_translation =  (Gdx.graphics.getWidth() - ((Gdx.graphics.getHeight()*16)/9))/2;
     }
 
     @Override
