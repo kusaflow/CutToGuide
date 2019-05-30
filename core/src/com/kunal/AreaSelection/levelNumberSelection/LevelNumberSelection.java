14c94a7ecd1ad3996aca84d223e7e703085fc55c
kunal sagar <kunalsagar1999@gmail.com>
2019-05-30 16:36:46 +0530

    file modified

--- core/src/com/kunal/AreaSelection/levelNumberSelection/LevelNumberSelection.java
+++ core/src/com/kunal/AreaSelection/levelNumberSelection/LevelNumberSelection.java
@@ -296,7 +296,7 @@ public class LevelNumberSelection implements Screen {
         levelTrack = 0;
         for (int i =0, inci = 0; i<TotalLevel/10; i++){
             for (int j =0, incj=470; j<2; j++){
-                for (int k =0, inck=240; k<5;k++){
+                for (int k =0, inck=250; k<5;k++){
                     levelTrack++;
                     if (levelTrack <= UnlockedLevel){
 
