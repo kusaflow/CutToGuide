673b15944c11b97abb449487586e3ffc563d650e
kunal sagar <kunalsagar1999@gmail.com>
2019-05-31 22:43:11 +0530

    new java class and some images

---
+++ core/src/com/kunal/AreaSelection/levelNumberSelection/TestClassToUnlockLevel.java
@@ -0,0 +1,47 @@
+package com.kunal.AreaSelection.levelNumberSelection;
+
+import com.badlogic.gdx.Screen;
+import com.kunal.MainGame;
+
+public class TestClassToUnlockLevel implements Screen {
+    MainGame game;
+
+    public TestClassToUnlockLevel(MainGame game) {
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
