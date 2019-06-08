b688efbe74e7fedb054861fd8ae704f8eaf2ff2e
kunal sagar <kunalsagar1999@gmail.com>
2019-06-08 15:46:43 +0530

    temp map is cpnverted to polygon shape

--- core/src/com/kunal/utils/TiledMapLoadingHelper.java
+++ core/src/com/kunal/utils/TiledMapLoadingHelper.java
@@ -9,7 +9,7 @@ public class TiledMapLoadingHelper {
     public static short NumberOfObj(){
         //testing is area 101 and level 101
         if (AllVariables.PresentLevelNumber == 101 && AllVariables.PresentAreaNumber == 101){
-            return 5;
+            return 10;
         }
 
         return 0;
