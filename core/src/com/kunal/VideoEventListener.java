2337335fbb2ba5ec3b372d7b799fd1e50ac9ca0a
kunal sagar <kunalsagar1999@gmail.com>
2019-07-12 14:34:02 +0530

    joined

--- core/src/com/kunal/VideoEventListener.java
+++ core/src/com/kunal/VideoEventListener.java
@@ -2,6 +2,8 @@ package com.kunal;
 
 public interface VideoEventListener {
     void onRewardedEvent(String type, int amount);
+
     void onRewardedVideoAdLoadedEvent();
+
     void onRewardedVideoAdClosedEvent();
 }
