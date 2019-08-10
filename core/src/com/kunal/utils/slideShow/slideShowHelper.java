833a0a0ce3e0f7c46cf237ccef098b651a459673
Kunal SAgar <kunalsagar1999@gmail.com>
2019-08-10 12:46:02 +0530

    shapechooser doing well

--- core/src/com/kunal/utils/slideShow/slideShowHelper.java
+++ core/src/com/kunal/utils/slideShow/slideShowHelper.java
@@ -31,7 +31,43 @@ public class slideShowHelper {
             }else if (focusImg == 5){
                 msg= "       This is ShapeChooser. It is used to pick shapes and powerups \n                                                                  to place in the level.";
             }else if (focusImg == 6){
-                msg="                                                            This is the ShapeChoosing Screen";
+                msg="                                  This is the ShapeChoosing Screen. \n                                                 All the shapes and powerups are present here.";
+            }else if (focusImg == 7) {
+                msg = "                           Touching this will bring you to the cutting sheet.";
+            }else if (focusImg == 8) {
+                msg = "                           Use your imagination to create pieces out of it.";
+            }else if (focusImg == 9) {
+                msg = "                           You can start Cutting with the green points Only.";
+            }else if (focusImg == 10) {
+                msg = "                                                                  Like this.";
+            }else if (focusImg == 11) {
+                msg = "                             You can start with any green point and land \n                                          anywhere you want";
+            }else if (focusImg == 12) {
+                msg = "                                                                 Like this.";
+            }else if (focusImg == 13) {
+                msg = "                                You cannot start and end with same point.";
+            }else if (focusImg == 14) {
+                msg = "                                  But can move like this, green to red.";
+            }else if (focusImg == 15) {
+                msg = "                                   You cannot start with any red point.";
+            }else if (focusImg == 16) {
+                msg = "                                   You can start with only green points.";
+            }else if (focusImg == 17) {
+                msg = "                                    Touch this to reset the sheet.";
+            }else if (focusImg == 18) {
+                msg = "                                                                 Like this.";
+            }else if (focusImg == 19) {
+                msg = "                   When you are done with cutting touch this to continue.";
+            }else if (focusImg == 20) {
+                msg = "                              Your cutouts are present like this in the grid.";
+            }else if (focusImg == 21) {
+                msg = "                                        You can reset from here too.";
+            }else if (focusImg == 22) {
+                msg = "                                                                 Like this.";
+            }else if (focusImg == 23) {
+                msg = "                  Touch any grid and this faded yellow rectangle \n                                                      will appear to your choice.";
+            }else if (focusImg == 24) {
+                msg = "              With the yellow rectangle at your shape touch this \n                                                        to move the shape to mainScreen";
             }
         }
 
