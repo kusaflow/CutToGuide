673c109a687486217fe910002bcf3c1bf61e8a1c
Kunal SAgar <kunalsagar1999@gmail.com>
2019-08-26 12:32:20 +0530

    shapechoose shapefix
    Former-commit-id: c0e8ecc9fe221a61e3e9024ebee386f0eaaebf07

--- android/src/com/kunal/AndroidLauncher.java
+++ android/src/com/kunal/AndroidLauncher.java
@@ -37,8 +37,14 @@ public class AndroidLauncher extends AndroidApplication implements AdVideoInterf
         super.onCreate(savedInstanceState);
         AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
 
+        //---------------------------------need to check------------------------------------------------
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        //----------------------------------------------------------------------------------------------
         //initialize(new MainGame(this), config);
 
         //ad banner ---------------------------------------------------------------------------------
