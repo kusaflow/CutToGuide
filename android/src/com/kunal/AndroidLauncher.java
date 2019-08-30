1417b2c57cb297d4c647e4c1e6df1eb6cd04a32f
Kunal SAgar <kunalsagar1999@gmail.com>
2019-08-30 20:29:21 +0530

    done lot of things 1.double tap on back to exit     2.hide the navigation bar permanently    3.credit created

--- android/src/com/kunal/AndroidLauncher.java
+++ android/src/com/kunal/AndroidLauncher.java
@@ -7,6 +7,7 @@ import android.content.pm.PackageManager;
 import android.graphics.Point;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.Display;
 import android.view.View;
@@ -35,7 +36,11 @@ public class AndroidLauncher extends AndroidApplication implements AdVideoInterf
 
     Boolean is_video_ad_loaded = false;
 
-	@Override
+
+    boolean doubleBackToExitPressedOnce = false;
+
+
+    @Override
 	protected void onCreate (Bundle savedInstanceState) {
 	    //wasan updated the code
         super.onCreate(savedInstanceState);
@@ -87,18 +92,54 @@ public class AndroidLauncher extends AndroidApplication implements AdVideoInterf
         setupRewarded();
 
         //---------------------------------need to check------------------------------------------------
-        View decorView = getWindow().getDecorView();
-        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN;
-        decorView.setSystemUiVisibility(uiOptions);
+        final int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
 
+        getWindow().getDecorView().setSystemUiVisibility(flags);
+        final View decorView = getWindow().getDecorView();
+        decorView
+                .setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener()
+                {
+
+                    @Override
+                    public void onSystemUiVisibilityChange(int visibility)
+                    {
+                        if((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0)
+                        {
+                            decorView.setSystemUiVisibility(flags);
+                        }
+                    }
+                });
         //----------------------------------------------------------------------------------------------
 
 
     }
 
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
 
-	//rewared ad video
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
+    }
+
+
+    //rewared ad video
     public void loadRewardedVideoAd(){
         adRewardedVideoView.loadAd(REWARDED_VIDEO_AD_UNIT_ID, new AdRequest.Builder().build());
     }
--- core/src/com/kunal/credit/credits.java
+++ core/src/com/kunal/credit/credits.java
@@ -17,7 +17,7 @@ public class credits implements Screen {
     OrthographicCamera cam;
     Viewport port;
 
-    private Texture cross;
+    private Texture cross, insta, twitter;
 
 
     public credits(MainGame game){
@@ -30,6 +30,8 @@ public class credits implements Screen {
 
         cross = new Texture(Gdx.files.internal("utils/hudX.png"));
 
+        insta = new Texture(Gdx.files.internal("referenceAppLogos/insta.png"));
+        twitter = new Texture(Gdx.files.internal("referenceAppLogos/Twitter.png"));
 
 
     }
@@ -43,7 +45,7 @@ public class credits implements Screen {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0.1f, 0.1f, 0.3f, 1f);
+        Gdx.gl.glClearColor(0.1f, 0.3f, .1f, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         AllVariables.batch.setProjectionMatrix(cam.combined);
         input();
@@ -53,17 +55,53 @@ public class credits implements Screen {
         AllVariables.batch.begin();
         AllVariables.batch.draw(cross,0+cam.position.x-AllVariables.WIDTH/2,720-128);
 
+        AllVariables.bitmapFont.draw(AllVariables.batch,"Hello, \n             |/\n", 100+100,600);
+        AllVariables.bitmapFont.draw(AllVariables.batch,"     I m |\\unal and hope you are enjoying this game :) ", 102+100,560);
+        AllVariables.bitmapFont.draw(AllVariables.batch,"             .", 104+100,571);
+        AllVariables.bitmapFont.draw(AllVariables.batch,"Follow us at :", 104+100,400);
+
+        AllVariables.batch.draw(insta, 200, 200, 100, 100);
+        AllVariables.batch.draw(twitter, 700, 200, 100, 100);
+
+
+        AllVariables.bitmapFont.draw(AllVariables.batch,"@kusaflow", 350,250);
+        AllVariables.bitmapFont.draw(AllVariables.batch,"@kusaflow", 850,250);
+
+
+
+
+
+
         AllVariables.batch.end();
     }
 
     private void input(){
         if (Gdx.input.justTouched()){
+            System.out.println(Gdx.input.getX()+"\t"+Gdx.input.getY());
+
             //cross
             if (Gdx.input.getX() >= (0 * AllVariables.inpM) + AllVariables.witdth_translation &&
                     Gdx.input.getX() < (128 * AllVariables.inpM) + AllVariables.witdth_translation &&
                     Gdx.input.getY() >= 0 * AllVariables.inpM && Gdx.input.getY() < 128 * AllVariables.inpM) {
                 //dispose();
                 game.setScreen(new AreaSelection(game));
+                return;
+            }
+
+            //insta
+            if (Gdx.input.getX() >= (187 * AllVariables.inpM) + AllVariables.witdth_translation &&
+                    Gdx.input.getX() < (525* AllVariables.inpM) + AllVariables.witdth_translation &&
+                    Gdx.input.getY() >= 400 * AllVariables.inpM && Gdx.input.getY() < 552 * AllVariables.inpM) {
+                //dispose();
+                AllVariables.openApps.OpenApp("https://www.instagram.com/kusaflow/?igshid=8y5b86e3yp6f");
+                return;
+            }
+            //twitter
+            if (Gdx.input.getX() >= (685* AllVariables.inpM) + AllVariables.witdth_translation &&
+                    Gdx.input.getX() < (1052* AllVariables.inpM) + AllVariables.witdth_translation &&
+                    Gdx.input.getY() >= 400 * AllVariables.inpM && Gdx.input.getY() < 552 * AllVariables.inpM) {
+                //dispose();
+                AllVariables.openApps.OpenApp("https://twitter.com/kusaflow?s=08");
                 return;
             }
         }
