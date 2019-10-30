0859d91bbeb9a9a2f85cf99896895788ab43b09c
Kunal SAgar <kunalsagar1999@gmail.com>
2019-10-30 11:30:36 +0530

    backu[

--- core/src/com/kunal/PlayGround/TypeThreeArea/ParticleManagerArea2.java
+++ core/src/com/kunal/PlayGround/TypeThreeArea/ParticleManagerArea2.java
@@ -6,15 +6,46 @@ import com.kunal.AllVariables;
 
 public class ParticleManagerArea2 {
 
+    ParticleEffect frontTyreFire, backTyreFire;
 
     public ParticleManagerArea2(){
+        frontTyreFire = new ParticleEffect();
+        frontTyreFire.load(Gdx.files.internal("particles/bicycleFire/tyreFire.p"), Gdx.files.internal(""));
+        //frontTyreFire.getEmitters().first().setPosition(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
+        frontTyreFire.start();
+
+
+        backTyreFire = new ParticleEffect();
+        backTyreFire.load(Gdx.files.internal("particles/bicycleFire/tyreFire.p"), Gdx.files.internal(""));
+        //frontTyreFire.getEmitters().first().setPosition(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
+        backTyreFire.start();
 
     }
 
     public void update(){
+        frontTyreFire.getEmitters().first().setPosition(AllVariables.FrontWheel.getPosition().x*AllVariables.PPM-20,
+                AllVariables.FrontWheel.getPosition().y*AllVariables.PPM-20);
+        backTyreFire.getEmitters().first().setPosition(AllVariables.BackWheel.getPosition().x*AllVariables.PPM -20,
+                AllVariables.BackWheel.getPosition().y*AllVariables.PPM-20);
+
+        frontTyreFire.update(Gdx.graphics.getDeltaTime());
+        backTyreFire  .update(Gdx.graphics.getDeltaTime());
+
+
+        if (frontTyreFire.isComplete())
+            frontTyreFire.reset();
+
+        if (backTyreFire.isComplete())
+            backTyreFire.reset();
+
+
     }
 
     public void render(){
+        AllVariables.batch.begin();
+        frontTyreFire.draw(AllVariables.batch);
+        backTyreFire.draw(AllVariables.batch);
+        AllVariables.batch.end();
 
     }
 }
