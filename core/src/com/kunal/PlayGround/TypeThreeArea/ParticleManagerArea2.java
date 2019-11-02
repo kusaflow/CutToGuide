12f8a1ea1dda4e6158c70f2752bf26ee0ee7f54a
Kunal SAgar <kunalsagar1999@gmail.com>
2019-11-02 13:27:10 +0530

    onfire done

--- core/src/com/kunal/PlayGround/TypeThreeArea/ParticleManagerArea2.java
+++ core/src/com/kunal/PlayGround/TypeThreeArea/ParticleManagerArea2.java
@@ -7,7 +7,7 @@ import com.kunal.PlayGround.VariablesForPlayArea;
 
 public class ParticleManagerArea2 {
 
-    ParticleEffect frontTyreFire, backTyreFire;
+    ParticleEffect frontTyreFire, backTyreFire,bar1;
 
     public ParticleManagerArea2(){
         frontTyreFire = new ParticleEffect();
@@ -21,6 +21,12 @@ public class ParticleManagerArea2 {
         //frontTyreFire.getEmitters().first().setPosition(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
         backTyreFire.start();
 
+        bar1 = new ParticleEffect();
+        bar1.load(Gdx.files.internal("particles/bicycleFire/bar1.p"), Gdx.files.internal(""));
+        //frontTyreFire.getEmitters().first().setPosition(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
+        bar1.start();
+
+
     }
 
     public void update(){
@@ -29,16 +35,21 @@ public class ParticleManagerArea2 {
                     AllVariables.FrontWheel.getPosition().y * AllVariables.PPM - 20);
             backTyreFire.getEmitters().first().setPosition(AllVariables.BackWheel.getPosition().x * AllVariables.PPM - 20,
                     AllVariables.BackWheel.getPosition().y * AllVariables.PPM - 20);
+            bar1.getEmitters().first().setPosition(AllVariables.rod6.getPosition().x * AllVariables.PPM ,
+                    AllVariables.rod6.getPosition().y * AllVariables.PPM-20);
 
             frontTyreFire.update(Gdx.graphics.getDeltaTime());
             backTyreFire.update(Gdx.graphics.getDeltaTime());
-
+            bar1.update(Gdx.graphics.getDeltaTime());
 
             if (frontTyreFire.isComplete())
                 frontTyreFire.reset();
 
             if (backTyreFire.isComplete())
                 backTyreFire.reset();
+
+            if (bar1.isComplete())
+                bar1.reset();
         }
 
 
@@ -49,6 +60,7 @@ public class ParticleManagerArea2 {
             AllVariables.batch.begin();
             frontTyreFire.draw(AllVariables.batch);
             backTyreFire.draw(AllVariables.batch);
+            bar1.draw(AllVariables.batch);
             AllVariables.batch.end();
         }
 
